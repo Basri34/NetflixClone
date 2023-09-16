e500ca4647cdc3f9581e8e1911b8175b146c9a8f
Omar Albasri <omar.albasri.2021@uni.strath.ac.uk>
2023-09-16 14:10:02 +0100

    Added Row and Some Firebase Functionality

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBsnENYNslQQeiONYnBeUlL9RIKBgxiYmM",
+  authDomain: "netflix-clone-90e5d.firebaseapp.com",
+  projectId: "netflix-clone-90e5d",
+  storageBucket: "netflix-clone-90e5d.appspot.com",
+  messagingSenderId: "836653828102",
+  appId: "1:836653828102:web:7bb81e62def7d90f0adb87"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
